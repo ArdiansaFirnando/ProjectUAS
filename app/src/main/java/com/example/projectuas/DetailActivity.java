37683ebd0958f3cf058764f182085fb4ea92773c
Ardiansa Firnando <ardiansa312@gmail.com>
2021-06-06 11:50:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectuas/DetailActivity.java
@@ -0,0 +1,75 @@
+package com.example.projectuas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.projectuas.fragment.Fragment_home;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private Bundle bundle;
+    private TextView txt_name;
+    private TextView txt_description;
+    private ImageView btn_back;
+    private ImageView img_image;
+    private ImageView btn_saved;
+    private Button btn_end;
+
+    private String name;
+    private String description;
+    private int image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        txt_name = findViewById(R.id.txt_name_detail);
+        txt_description = findViewById(R.id.txt_description_detail);
+        img_image = findViewById(R.id.img_image_detail);
+        btn_back = findViewById(R.id.btn_back_detail);
+        btn_saved = findViewById(R.id.btn_saved_detail);
+        btn_end = findViewById(R.id.btn_end_detal);
+
+        bundle = getIntent().getExtras();
+        if (bundle != null){
+            name = bundle.getString("name");
+            description = bundle.getString("description");
+            image = bundle.getInt("image");
+        }
+
+        txt_name.setText(name);
+        txt_description.setText(description);
+        img_image.setImageResource(image);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
+
+        btn_saved.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/projectuas/adapter/TeamsAdapter.java
@@ -0,0 +1,140 @@
+package com.example.projectuas.adapter;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.projectuas.R;
+import com.example.projectuas.model.ModelTeams;
+
+import java.util.ArrayList;
+
+import javax.security.auth.callback.Callback;
+
+public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.TeamsViewHolder> {
+
+    private ArrayList<ModelTeams> listTeams;
+    private Callback callback;
+    private int posku;
+    private View viewku;
+
+    public interface Callback{
+        void onClick(int position);
+    }
+
+    public TeamsAdapter(ArrayList<ModelTeams> listTeams, Callback callback) {
+        this.listTeams = listTeams;
+        this.callback = callback;
+    }
+
+    @NonNull
+    @Override
+    public TeamsAdapter.TeamsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item_teams, parent, false);
+        return new TeamsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TeamsAdapter.TeamsViewHolder holder, int position) {
+
+        holder.img_image.setImageResource(listTeams.get(position).getImage());
+        holder.txt_name.setText(listTeams.get(position).getName());
+        holder.txt_years.setText(listTeams.get(position).getYears());
+        holder.txt_country.setText(listTeams.get(position).getCountry());
+        holder.txt_description.setText(listTeams.get(position).getDescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listTeams.size();
+    }
+
+    public class TeamsViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
+
+        private TextView txt_name;
+        private TextView txt_years;
+        private TextView txt_country;
+        private TextView txt_description;
+        private ImageView img_image;
+        private CardView cardView;
+
+        public TeamsViewHolder(View view){
+            super(view);
+
+            viewku = view;
+
+            txt_name = view.findViewById(R.id.txt_name);
+            txt_years = view.findViewById(R.id.txt_years);
+            txt_country = view.findViewById(R.id.txt_country);
+            txt_description = view.findViewById(R.id.txt_description);
+            img_image = view.findViewById(R.id.img_image);
+            cardView = view.findViewById(R.id.id_cardview);
+
+            cardView.setOnCreateContextMenuListener(this);
+
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    callback.onClick(getLayoutPosition());
+                }
+            });
+
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            MenuItem Delete = menu.add(Menu.NONE, 1, 1, "Delete");
+            posku = getLayoutPosition();
+            Delete.setOnMenuItemClickListener(onlickcontextmenu);
+        }
+    }
+
+    private final MenuItem.OnMenuItemClickListener onlickcontextmenu = new MenuItem.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+
+            switch (item.getItemId()) {
+                case 1:
+                    //Delete data, butuh konfirmasi dialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(viewku.getContext());
+                    builder.setMessage("Are you sure you want to delete data?")
+                            .setCancelable(false)
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    listTeams.remove(posku);
+                                    notifyDataSetChanged();
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            })
+                            //Set your icon here
+                            .setTitle(listTeams.get(posku).getName())
+                            .setIcon(listTeams.get(posku).getImage());
+                    AlertDialog alert = builder.create();
+                    alert.show();//showing the dialog
+
+                    break;
+            }
+            return true;
+        }
+    };
+}
